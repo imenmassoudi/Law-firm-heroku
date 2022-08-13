de4db3952d28feea3039938792c0e64595cd1856
imen massoudi <imencharadi51@gmail.com>
2022-08-13 13:47:10 +0100

    make it better

--- backend/server.js
+++ backend/server.js
@@ -69,14 +69,14 @@ app.use('/parametre', parametreRouter);
 
 //serve static assets
 
-/*if(process.env.NODE_ENV === 'production'){
+if(process.env.NODE_ENV === 'production'){
     app.use(express.static('build'));
 
     app.get('*',(req,res)=>{
             res.sendFile(path.resolve(__dirname,'build','index.html'))
     })
 
-}*/
+}
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
