429df16cdc481c81b03dc656807b3903f4b22a67
imen massoudi <imencharadi51@gmail.com>
2022-08-14 18:58:50 +0100

    make it better

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 const uri = process.env.ATLAS_URI;
 //serve static assets
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('build'));
+    app.use(express.static(path.join(__dirname, 'build')));
 
     app.get('*',(req,res)=>{
         res.sendFile(path.resolve(__dirname,'build','index.html'))
