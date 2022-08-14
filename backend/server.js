ab09736cfaa5ec96ae3bef9760889c54d5cc7435
imen massoudi <imencharadi51@gmail.com>
2022-08-14 19:40:55 +0100

    make it better

--- backend/server.js
+++ backend/server.js
@@ -29,20 +29,20 @@ mongoose
     .catch(err => console.log(err));
 
 app.enable('trust proxy')
-const usersRouter = require(__dirname+'/routes/users');
-const tribRouter = require(__dirname+'/routes/tribunaux');
-const clientRouter = require(__dirname+'/routes/clients');
-const serviceRouter = require(__dirname+'/routes/services');
-const emplacementRouter = require(__dirname+'/routes/emplacements');
-const dossierRouter = require(__dirname+'/routes/dossiers');
-const archiveRouter = require(__dirname+'/routes/archives');
-const demandeursRouter = require(__dirname+'/routes/demandeurs');
-const tacheRouter = require(__dirname+'/routes/taches');
-const honoraireRouter = require(__dirname+'/routes/honoraires');
-const ssDossierRouter = require(__dirname+'/routes/sousDossiers');
-const cRouter = require(__dirname+'/routes/collaborateursReg');
+const usersRouter = require(__dirname+'/routes/Users');
+const tribRouter = require(__dirname+'/routes/Tribunaux');
+const clientRouter = require(__dirname+'/routes/Clients');
+const serviceRouter = require(__dirname+'/routes/Services');
+const emplacementRouter = require(__dirname+'/routes/Emplacements');
+const dossierRouter = require(__dirname+'/routes/Dossiers');
+const archiveRouter = require(__dirname+'/routes/Archives');
+const demandeursRouter = require(__dirname+'/routes/Demandeurs');
+const tacheRouter = require(__dirname+'/routes/Taches');
+const honoraireRouter = require(__dirname+'/routes/Honoraires');
+const ssDossierRouter = require(__dirname+'/routes/SousDossiers');
+const cRouter = require(__dirname+'/routes/CollaborateursReg');
 
-const ReglementsRouter = require(__dirname+'/routes/reglements');
+const ReglementsRouter = require(__dirname+'/routes/Reglements');
 app.use('/reglements', ReglementsRouter);
 app.use('/demandeur', demandeursRouter);
 app.use('/tache', tacheRouter);
@@ -58,23 +58,23 @@ app.use('/archives', archiveRouter);
 app.use('/honoraires', honoraireRouter);
 app.use('/sousdossier', ssDossierRouter);
 
-const collaborateursRouter = require(__dirname+'/routes/collaborateurs');
+const collaborateursRouter = require(__dirname+'/routes/Collaborateurs');
 
 app.use('/collaborateurs', collaborateursRouter);
 
-const greffiersRouter = require(__dirname+'/routes/greffiers');
+const greffiersRouter = require(__dirname+'/routes/Greffiers');
 
 app.use('/greffiers', greffiersRouter);
 
-const primegreffiersRouter = require(__dirname+'/routes/primegreffiers');
+const primegreffiersRouter = require(__dirname+'/routes/Primegreffiers');
 
 app.use('/primegreffiers', primegreffiersRouter);
 
-const typedossiersRouter = require(__dirname+'/routes/typedossiers');
+const typedossiersRouter = require(__dirname+'/routes/Typedossiers');
 
 app.use('/typedossiers', typedossiersRouter);
 
-const parametreRouter = require(__dirname+'/routes/parametre');
+const parametreRouter = require(__dirname+'/routes/Parametre');
 app.use('/parametre', parametreRouter);
 
 
