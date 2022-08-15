5d52928f40a6d89c5e2fefd85a2cb1e03f50a75b
imen massoudi <imencharadi51@gmail.com>
2022-08-15 11:31:50 +0100

    'ok'

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const path = require('path')
 require('dotenv').config();
 //express server
 const app = express();
-const port = 5000 || process.env.PORT;
+const port = process.env.PORT || 5000;
 //middleware
 app.use(cors());
 app.use(express.json());
